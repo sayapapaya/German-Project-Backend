6ad6714561c0789ba08e9bba68cf102f24644313
Katrine Tjoelsen <ktjolsen@mit.edu>
2015-10-05 21:03:39 -0400

    Put key functionality into functions.

--- extract_features.py
+++ extract_features.py
@@ -10,29 +10,36 @@ from pattern.de import gender, MALE, FEMALE, NEUTRAL
 
 
 # read text and create blob object
-file_path = "Articles/Aschenputtel"
-f = open(file_path)
-article = f.read().decode('utf-8')
-blob = TextBlob(article, parser=PatternParser(pprint=True, lemmata=True))
-
+file_name = "Articles/Aschenputtel"
+def get_blob(file_path):
+    f = open(file_path)
+    article = f.read().decode('utf-8')
+    blob = TextBlob(article, parser=PatternParser(pprint=True, lemmata=True))
+    return blob
 
 # extract average number of words per sentence
-sentences = blob.sentences
-average_sentence_length = np.mean(np.array([len(sentence.words) for sentence in sentences]))
-
+def get_avg_sentence_length(blob):
+    sentences = blob.sentences
+    average_sentence_length = np.mean(np.array([len(sentence.words) for sentence in sentences]))
+    return average_sentence_length
 
 # extract average word length
-words = blob.words
-average_word_length = np.mean(np.array([len(word) for word in words]))
-
+def get_avg_word_length(blob):
+    words = blob.words
+    average_word_length = np.mean(np.array([len(word) for word in words]))
+    return average_word_length
 
 # count number of words with each part of speech tag
-tags = blob.tags
-number_of_tags = float(len(tags))
-unique_tags = set([t[1] for t in tags])
-number_of_each_tag = {pos: sum([1 for t in tags if t[1] == pos]) for pos in unique_tags}
-relative_frequency_of_each_tag = {k : v / number_of_tags for k, v in number_of_each_tag.items()}
+def get_relative_frequency_of_pos_tags(blob):
+    tags = blob.tags
+    number_of_tags = float(len(tags))
+    unique_tags = set([t[1] for t in tags])
+    number_of_each_tag = {pos: sum([1 for t in tags if t[1] == pos]) for pos in unique_tags}
+    relative_frequency_of_each_tag = {k : v / number_of_tags for k, v in number_of_each_tag.items()}
+    return relative_frequency_of_each_tag
 
+
+blob = get_blob(file_name)
 
 # For more on POS tags: https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html 
 # If used as feature, combine certain dimensions, and make dimensions be .e.g. relative frequency of nouns or verbs etc.? 
