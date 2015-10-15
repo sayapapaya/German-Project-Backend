05d4b303232971b06cac99f1d1d9c09fc926fae6
Katrine Tjoelsen <ktjolsen@mit.edu>
2015-10-15 12:38:40 -0400

    Uses dict mapping article names to feature vectors instead

--- recommend_article.py
+++ recommend_article.py
@@ -6,8 +6,6 @@ from sklearn.neighbors import NearestNeighbors
 from plot_features_vs_readability import get_goethe_article_names_with_scores
 
 
-
-
 ## TODO: UPDATE TO REFLECT WHICH FEATURES SHOW GOOD CORRELATION
 def create_feature_vector(blob):
     """
@@ -21,26 +19,23 @@ def create_feature_vector(blob):
         extract_features.get_relative_frequency_of_verbs(blob),
         extract_features.proportion_of_sentences_that_contain_past_participle(blob)
     ])
-
     return feature_vector
 
-
-def create_all_feature_vectors_of_articles(article_directory, article_names):
+def create_feature_vector_dict(article_directory, article_names):
     """
     article_directory -- string path to directory containing articles
     article_names -- list of string names of articles in article directory
     
-    return numpy array containing feature vectors in same order as
-        articles appear in article_names
+    return dictionary mapping article names to feature vectors
     """
-    feature_vectors = []
+    article_name_to_feature_vectors = {}
 
     for article_name in article_names:
         article_path = "".join([article_directory, "/", article_name])
         blob = extract_features.get_blob(article_path)
-        feature_vectors.append(create_feature_vector(blob))
+        article_name_to_feature_vectors[article_name] = create_feature_vector(blob)
     
-    return np.array(feature_vectors)
+    return article_name_to_feature_vectors
 
 
 def recommend_k_articles(article_names, articles, user, k):
